c2af3254a2d65a7474137865da1a95cd1f74764e
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-02-12 01:46:54 +0000

    assest and metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,28 @@
 import type { Metadata } from "next";
-// eslint-disable-next-line camelcase
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import React from "react";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const inter = localFont({
+  src: "./fonts/interVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 700 800 900",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const spaceGrotesk = localFont({
+  src: "./fonts/SpaceGroteskVF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 700",
 });
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "A better version of stack overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -27,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl font-black text-black">Tailwin CSS is fun!</h1>
+    <>
+      <h1 className="text-3xl font-black text-black">Tailwind CSS is fun!</h1>
+    </>
   );
 };
 
